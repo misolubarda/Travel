81dc1ec0addae7d5f37c3dda7b8eb05bf75ca189
Miso Lubarda <miso.lubarda@gmail.com>
2016-08-20 11:29:00 +0200

    Initial Commit

---
+++ Travel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Travel
+//
+//  Created by Miso Lubarda on 20/08/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Travel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Travel
+//
+//  Created by Miso Lubarda on 20/08/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
