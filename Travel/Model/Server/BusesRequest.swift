8de233d86041e048fd9ca89de2d6118e690242a8
Miso Lubarda <miso.lubarda@gmail.com>
2016-08-20 11:55:16 +0200

    added class for busses request

--- Travel/Model/Server/BusesRequest.swift
+++ Travel/Model/Server/BusesRequest.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+@objc class BusesAPIRequest: NSObject, APIRequestProtocol {
+    
+    /// Required by protocol (see APIRequestProtocol)
+    var baseURL: NSURL
+    /// Required by protocol (see APIRequestProtocol)
+    var responseQueue: dispatch_queue_t
+    /// Required by protocol (see APIRequestProtocol)
+    var dataTask: NSURLSessionDataTask? = nil
+    /// Required by protocol (see APIRequestProtocol)
+    var endpointPath: String {
+        return "37yzm"
+    }
+    
+    /// Required by protocol (see APIRequestProtocol)
+    var responseParsing: ((data: NSData) throws -> String) = { (data) in
+        return ""
+    }
+    
+    /**
+     Initializes request with query string.
+     
+     - parameter baseURL: Base url of the request.
+     - parameter responseQueu: Queue on which to provide response.
+     
+     - returns: Request for user query.
+     */
+    init(baseURL: NSURL, responseQueue: dispatch_queue_t) {
+        self.baseURL = baseURL
+        self.responseQueue = responseQueue
+    }
+}
+
+extension BusesAPIRequest {
+    enum BusesAPIRequestError: ErrorType {
+        case StringFromData, JSONResultsMissing
+    }
+}
